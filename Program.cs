36bfcae3316d776c60e2798628f867a49370f97e
idarkoo <48064623+idarkoo@users.noreply.github.com>
2019-03-07 11:31:26 +0100

    done

--- Program.cs
+++ Program.cs
@@ -10,14 +10,15 @@ namespace DomashnaG2DarkoIvanovski
     {
         static void Main(string[] args)
         {
+            #region task 2 and 3
             Console.Write("Enter 20 numbers:");
             int[] arrNumbers = new int[20];
-            
+
             for (var i = 0; i < arrNumbers.Length; i++)
             {
                 arrNumbers[i] = int.Parse(Console.ReadLine());
             }
-            Console.Write("Here are the numbers you entered");
+            Console.WriteLine("Here are the numbers you entered");
 
             for (int i = 0; i < arrNumbers.Length; i++)
             {
@@ -28,12 +29,34 @@ namespace DomashnaG2DarkoIvanovski
                     Console.WriteLine("Skipped");
                     continue;
                 }
-                if(Math.Abs(arrNumbers[i]).ToString().Length == 3)
+                if (Math.Abs(arrNumbers[i]).ToString().Length == 3)
                 {
                     break;
                 }
             }
             Console.ReadLine();
+            #endregion
+            #region task 1
+            int[] intArray = new int[5];
+            Console.WriteLine("Enter first number:");
+            intArray[0] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number:");
+            intArray[1] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter third number:");
+            intArray[2] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter fourth number:");
+            intArray[3] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter fifth number:");
+            intArray[4] = Convert.ToInt32(Console.ReadLine());
+            int result = 0;
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                result += intArray[i];
+            }
+            Console.WriteLine("The sum is: ");
+            Console.WriteLine(result);
+            Console.ReadLine();
+            #endregion
         }
     }
 }
